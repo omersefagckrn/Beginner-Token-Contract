2c7188ae96b91bb335f7b609e74988486562c037
Ömer Sefa Güçkıran <73169113+omersefagckrn@users.noreply.github.com>
2022-01-14 17:59:10 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -2,46 +2,59 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Token {
-    string public name;
-    string public symbol;
-    uint256 public decimals;
-    uint256 public totalSupply; // 1000000000000000000 -> 18 decimals.
+contract Token{
+    string private name;
+    string private symbol;
+    uint256 private decimals; // adet "1000" -> 1000 den sonra 9 sıfır eklenir.
+    uint256 private totalSupply; // 1000_000_000_000 -> 9 basamak.
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowed;
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed from,address indexed to,uint256 value);
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _decimals,
-        uint256 _totalSupply
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
+    constructor(){
+        name = "Omer Token";
+        symbol = "OTK";
+        decimals = 9;
+        totalSupply = 1000_000_000_000;
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value)
-        external
-        returns (bool success)
-    {
+    // remix üzerinden msg.sender kullanılırak yeni hesaba token iletimi sağlanır.
+    function transfer(address _to,uint256 _value) external returns(bool success){
         require(balanceOf[msg.sender] >= _value); // göndericek yeterli token var mı kontrol
-        _transfer(msg.sender, _to, _value);
+        require(_to != address(0));
+        balanceOf[msg.sender] -= (_value);
+        balanceOf[_to] += (_value);
+        emit Transfer(msg.sender,_to,_value);
         return true;
     }
 
-    function _transfer(
-        address _from,
-        address _to,
-        uint256 _value
-    ) internal {
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
+    // dinamik bir şekilde hesaptan hesaba token aktarımı gerçekleştirir.
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[_from] >= _value);
+        balanceOf[_from] -= (_value);
+        balanceOf[_to] += (_value);
         emit Transfer(_from, _to, _value);
+        return true;
     }
+
+    // çekilebilicek token sayısını sınırlar
+    function approve(address _spender, uint256 _value) public returns (bool success) { 
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // önce approve kullanılır, ardından ne kadar token göndermesine izin verdiğine bakabiliriz
+    function allowance(address _owner, address _spender) public view returns (uint256) { 
+        return allowed[_owner][_spender];
+    }
+
 }
+
+// 0x<firstAccount> sender
+// 0x<secendAccount> receiving(sınırlandırılan)
+// sınırlandırma veririsek <approve -> allowance> sınırlandırılan değerden daha fazla girerse hata fırlatır
